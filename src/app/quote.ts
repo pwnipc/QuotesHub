87559b66669de19ab6099daa995b32a852329734
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-10-11 20:36:27 +0300

    add highest vote property

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,9 +2,11 @@ export class Quote {
     showDescription!: boolean;
     qupvote!: number;
     qdownvote!: number;
+    highestVote! : boolean;
     constructor(public qid: number, public qtitle: string, public qauthor: string, public qsubmitter:string, public qdescription: string,  public postdate: Date ){
         this.showDescription = false;
         this.qupvote = 0;
         this.qdownvote = 0;
+        this.highestVote = false;
     }
 }
